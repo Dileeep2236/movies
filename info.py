1f7bbb9941778d520eca33d1d6df871c616cc19e
Dileeep2236 <d3898287@gmail.com>
2025-05-18 14:16:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21138525'))
 API_HASH = environ.get('API_HASH', '6b24067f5539b4d7f056b097656a3320')
-BOT_TOKEN = environ.get('BOT_TOKEN', ' 7970121547:AAH-HFVejGt9tvFu0SC1Kq_xnzcDIsM34S4')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7970121547:AAH-HFVejGt9tvFu0SC1Kq_xnzcDIsM34S4')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7310483382').split()]
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+wpjt1Kdc-V40ZT
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002237507587').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dileep:dileep2236@cluster0.kyybvpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dileep:dileep2236@@cluster0.kyybvpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
